96bae16a3e900d8640b4c8fa37f744acd62f23b9
zarif-al <zarif_al96@outlook.com>
2023-11-25 14:02:34 +0600

    refactor: re-organize solution

--- src/two_sum/mod.rs
+++ src/two_sum/mod.rs
@@ -4,37 +4,41 @@
  */
 use std::{collections::HashMap, vec};
 
-pub fn solution(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    // Create a map to store index of numbers.
-    let mut arg_map: HashMap<i32, i32> = HashMap::new();
+struct Solution {}
 
-    /*
-     * Loop over the numbers and substract the number from the current target, assign it
-     * to a variable 'remainder'.
-     *
-     * Check if our map has any key that matches the remainder.
-     * If yes, we have found our answer and we can return it.
-     *
-     * If no, we can insert the current number as the key and its index as the value in
-     * our map.
-     *
-     * Given the conditions of the problem, our array will always contain a pair of numbers that add
-     * up to the target, so we can be sure that we will find an answer by the end of the loop.
-     */
-    for (i, num) in nums.iter().enumerate() {
-        let remainder = target - num;
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        // Create a map to store index of numbers.
+        let mut arg_map: HashMap<i32, i32> = HashMap::new();
 
-        if arg_map.contains_key(&remainder) {
-            let remainder_index = *arg_map.get(&remainder).unwrap();
+        /*
+         * Loop over the numbers and substract the number from the current target, assign it
+         * to a variable 'remainder'.
+         *
+         * Check if our map has any key that matches the remainder.
+         * If yes, we have found our answer and we can return it.
+         *
+         * If no, we can insert the current number as the key and its index as the value in
+         * our map.
+         *
+         * Given the conditions of the problem, our array will always contain a pair of numbers that add
+         * up to the target, so we can be sure that we will find an answer by the end of the loop.
+         */
+        for (i, num) in nums.iter().enumerate() {
+            let remainder = target - num;
 
-            return vec![remainder_index, i as i32];
-        } else {
-            arg_map.insert(*num, i as i32);
+            if arg_map.contains_key(&remainder) {
+                let remainder_index = *arg_map.get(&remainder).unwrap();
+
+                return vec![remainder_index, i as i32];
+            } else {
+                arg_map.insert(*num, i as i32);
+            }
         }
-    }
 
-    // This part should be unreachable.
-    unreachable!();
+        // This part should be unreachable.
+        unreachable!();
+    }
 }
 
 #[cfg(test)]
@@ -46,7 +50,7 @@ mod tests {
         let number_array = [-3, 4, 3, 6].to_vec();
         let target = 0;
 
-        let test_1 = solution(number_array, target);
+        let test_1 = Solution::two_sum(number_array, target);
 
         assert_eq!(test_1, vec![0, 2]);
     }
