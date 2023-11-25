15cb81150dad87a402045024da442c4020e23b2a
zarif-al <zarif_al96@outlook.com>
2023-11-25 10:18:30 +0600

    docs: add problem link and explanation

--- src/two_sum/mod.rs
+++ src/two_sum/mod.rs
@@ -2,9 +2,26 @@
 
 use std::{collections::HashMap, vec};
 
+/**
+ *  Problem Link: @see https://leetcode.com/problems/two-sum/
+ */
 pub fn solution(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    // Create a map to store index of numbers.
     let mut arg_map: HashMap<i32, i32> = HashMap::new();
 
+    /*
+     * Loop over the numbers and substract the number from the current target, assign it
+     * to a variable 'remainder'.
+     *
+     * Check if our map has any key that matches the remainder.
+     * If yes, we have found our answer and we can return it.
+     *
+     * If no, we can insert the current number as the key and its index as the value in
+     * our map.
+     *
+     * Given the conditions of the problem, our array will always contain a pair of numbers that add
+     * up to the target, so we can be sure that we will find an answer by the end of the loop.
+     */
     for (i, num) in nums.iter().enumerate() {
         let remainder = target - num;
 
